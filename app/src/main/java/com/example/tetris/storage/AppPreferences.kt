57d46841b851676a43d711f6cc800e36325fd574
Alexey Yuriev <yuriev.alexei@mail.ru>
2023-03-31 10:23:00 +0300

    init commit

---
+++ app/src/main/java/com/example/tetris/storage/AppPreferences.kt
@@ -0,0 +1,21 @@
+package com.example.tetris.storage
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class AppPreferences(ctx: Context) {
+    private val strHighScore: String = "HIGH_SCORE"
+    var data: SharedPreferences = ctx.getSharedPreferences("APP_PREFERENCES", Context.MODE_PRIVATE)
+
+    private fun saveHighScore(highScore: Int){
+        data.edit().putInt(strHighScore, highScore).apply()
+    }
+
+    fun getHighScore(): Int{
+        return data.getInt(strHighScore, 0)
+    }
+
+    fun clearHighScore(){
+        saveHighScore(0)
+    }
+}
